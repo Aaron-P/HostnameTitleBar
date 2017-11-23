b4281c5dcfcac32e51956f000ee602ae8869641b
Aaron-P <aaron-p@users.noreply.github.com>
2017-11-23 00:44:31 -0600

    Move some stuff around.

--- HostnameTitleBar/package.bat
+++ HostnameTitleBar/package.bat
@@ -3,11 +3,11 @@ cd .\HostnameTitleBar
 
 rmdir /S /Q ..\Firefox
 ..\7za.exe a -tzip ..\Firefox\HostnameTitleBar.zip ^
+	..\..\LICENSE.md^
+	..\..\README.md^
 	background.js^
 	content.js^
 	icon.svg^
-	LICENSE.md^
 	manifest.json^
 	options.html^
-	options.js^
-	README.md
+	options.js
