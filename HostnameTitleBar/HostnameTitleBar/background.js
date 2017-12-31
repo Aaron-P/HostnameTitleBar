1c5e178d7e7dbd2dedd0c995cbc26fb001d2c17e
Aaron-P <aaron-p@users.noreply.github.com>
2017-12-31 09:48:22 -0600

    Add the option to prepend the tab title to the window title.

--- HostnameTitleBar/HostnameTitleBar/background.js
+++ HostnameTitleBar/HostnameTitleBar/background.js
@@ -1,38 +1,42 @@
 (function () {
     "use strict";
 
-    function setTitle(windowId, tabUrl) {
+    function setTitle(windowId, tab) {
         let url;
         try {
-            url = new URL(tabUrl);
+            url = new URL(tab.url);
         } catch (error) { }
 
+        let title = tab.title;
+
         //A suffix would be nicer but that isn't currently supported.
-        let update = { titlePreface: " " }; //For some reason titlePreface = "", null, or undefined doesn't clear it, but spaces seem to get trimmed so this works.
-        if (url && ["http:", "https:"].indexOf(url.protocol) !== -1) { //Other protocols?
-            update.titlePreface = url.hostname + " - "; //Customize?
-        }
-        browser.windows.update(windowId, update).then(null, error => console.error(error));
+        browser.storage.local.get({ prependTitle: false }).then(results => {
+            let update = { titlePreface: " " }; //For some reason titlePreface = "", null, or undefined doesn't clear it, but spaces seem to get trimmed so this works.
+            if (url && ["http:", "https:"].indexOf(url.protocol) !== -1) { //Other protocols?
+                update.titlePreface = (results.prependTitle && title ? title + " - " : "") + url.hostname + " - "; //Customize?
+            }
+            browser.windows.update(windowId, update).then(null, error => console.error(error));
+        });
     }
 
     //Set the title for all current windows.
     browser.windows.getAll({ windowTypes: ["normal", "popup"], populate: true }).then(windows => {
         windows.forEach($window => {
-            setTitle($window.id, $window.tabs.find(tab => tab.active).url);
+            setTitle($window.id, $window.tabs.find(tab => tab.active));
         });
     }, error => console.error(error));
 
     //Set the title when the active tab changes.
     browser.tabs.onActivated.addListener(activeInfo => {
         browser.tabs.get(activeInfo.tabId).then(tab => {
-            setTitle(activeInfo.windowId, tab.url);
+            setTitle(activeInfo.windowId, tab);
         }, error => console.error(error));
     });
 
     //Set the title when the active tab's url changes.
     browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         if (tab.active) {
-            setTitle(tab.windowId, tab.url);
+            setTitle(tab.windowId, tab);
         }
     });
 }());
--- HostnameTitleBar/HostnameTitleBar/options.js
+++ HostnameTitleBar/HostnameTitleBar/options.js
@@ -1,15 +1,24 @@
 (function () {
     "use strict";
 
-    let checkbox = document.getElementById("inject-title");
-    checkbox.addEventListener("change", function () {
+    let injectTitle = document.getElementById("inject-title");
+    injectTitle.addEventListener("change", function () {
         browser.storage.local.set({ injectTitle: this.checked }).then(null, error => console.error(error));
         //Do we want to send a message to already loaded tabs when the setting changes?  Probably not worth it.
     });
 
+    let prependTitle = document.getElementById("prepend-title");
+    prependTitle.addEventListener("change", function () {
+        browser.storage.local.set({ prependTitle: this.checked }).then(null, error => console.error(error));
+    });
+
     document.addEventListener("DOMContentLoaded", () => {
-        browser.storage.local.get({ injectTitle: false }).then(results => {
-            checkbox.checked = results.injectTitle;
+        browser.storage.local.get({
+            injectTitle: false,
+            prependTitle: false
+        }).then(results => {
+            injectTitle.checked = results.injectTitle;
+            prependTitle.checked = results.prependTitle;
         }, error => console.error(error));
     });
 }());
