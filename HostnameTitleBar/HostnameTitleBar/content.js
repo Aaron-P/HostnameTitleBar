3191f4849756532d880a6daa769cf05b8debd405
Aaron-P <aaron-p@users.noreply.github.com>
2019-03-09 01:09:28 -0600

    Move 3rd party licenses to separate file. Cleanup .gitignore.

--- HostnameTitleBar/HostnameTitleBar/content.js
+++ HostnameTitleBar/HostnameTitleBar/content.js
@@ -9,13 +9,13 @@
             // try {
             //     url = new URL(document.location.href);
             // } catch (error) { }
-    
+
             // if (url) {
             //     document.title = url.href.replace(new RegExp("^" + url.protocol + "//"), "")
             // } else {
             //     document.title = document.location.href;
             // }
-    
+
             //This only fixes the title on first load.  If a page deliberatly sets it's title to "" or deletes all the title tags later it will still cause the bug.
             //Do we want to actively monitor pages to make sure we keep a non-blank title?
             document.title = document.location.href;
